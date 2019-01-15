e7a4cd017bf3e1ee7b96ab034f6cabab61972495
楯山詩織 <shiori@tatesanshiorinoMacBook-Pro.local>
2019-01-15 15:11:00 +0900

    8-6

--- Practice/src/practice07/PTra07_03.java
+++ Practice/src/practice07/PTra07_03.java
@@ -16,7 +16,7 @@ public class PTra07_03 {
 
 
 		// ★ 配列keywordに格納されている値を後ろから順番に全て表示してください
-		for(int i =keyword.length-1; i >= 0;i--) {
+		for(int i =keyword.length-1; i >= 0; i--) {
 			System.out.println(keyword[i]);
 		}
 
--- Practice/src/practice08/PTra08_05.java
+++ Practice/src/practice08/PTra08_05.java
@@ -32,8 +32,20 @@ public class PTra08_05 {
 		int choice = scanner.nextInt();
 
 		// ★ 変数choiceの中が、1であれば四角形の面積を算出するメソッドを、2であれば三角形の面積を算出するメソッドを呼び出してください
+		switch(choice) {
+		case 1:
+			System.out.println(quadrangle(width, height));
+			break;
+		case 2:
+			System.out.println(triangle(width, height));
+			break;
 
-	}
+
+		}
+
+		}
+
+	
 
 	/*
 	 * ★ 以下の仕様でメソッドを作成してください
@@ -44,6 +56,12 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、四角形の面積を返すメソッド
 	 */
+	public static double quadrangle(double width, double height) {
+
+			double quadrangle=width*height;
+			return quadrangle;
+
+	}
 
 
 	/*
@@ -55,5 +73,8 @@ public class PTra08_05 {
 	 *
 	 * 引数で渡された横の長さ(width)と縦の長さ(height)から、三角形の面積を返すメソッド
 	 */
-
+	public static double triangle(double width, double height) {
+		double triangle=(width*height)/2;
+		return triangle;
+	}
 }
